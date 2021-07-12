35df33f98cce83f4f53837c1c1ea3fae501a05d8
Rediate15 <72558545+Rediate15@users.noreply.github.com>
2021-07-12 23:25:49 +0200

    Bugfixes und Teil eins der Positionierung
    Bugfixes mit den Informationstafeln
    Positionierung:
    Aufteilung in Generationen
    Sortierung in den Generationen nach Familien
    Zusammenführung von Ehepaaren

--- pages/scripts/resizelogic.js
+++ pages/scripts/resizelogic.js
@@ -4,6 +4,8 @@ const scaleFactor = 10; // minsize muss durch scaleFactor teilbar sein
 
 const resize = document.querySelector(".resize");
 
+positionElements();
+
 var scaleSize = convertAbsoluteUnitsToRelative();
 orientateAdditionalInfo();
 
@@ -16,6 +18,168 @@ if (resize.addEventListener) {
 else {
     resize.onwheel = scrollevent;
     resize.attachEvent("onmousedown", mousedown);
+}
+
+function positionElements(){
+    var personList = Array.from(document.getElementsByClassName("content"));
+    var generationList = [[], []];
+
+    for (var i=0; i<personList.length; i++){
+        if (personList[i].getAttribute("data-mutter") == "" && personList[i].getAttribute("data-vater") == ""){
+            generationList[1].push(personList[i]);
+            personList.splice(i, 1);
+            i--;
+        }
+    }
+
+    for (var i=0; i<generationList[1].length; i++) {
+        var currentId = generationList[1][i].getAttribute("data-id");
+        for (var j=0; j<personList.length; j++) {
+            if (personList[j].getAttribute("data-mutter") == currentId || personList[j].getAttribute("data-vater") == currentId || personList[j].getAttribute("data-ehepartner") == currentId) {
+                generationList[0].push(generationList[1][i]);
+                generationList[1].splice(i, 1);
+                i--;
+                break;
+            }
+        }
+    }
+
+
+    while (personList.length > 0) {
+        generationList.push([]);
+        for (var i=0; i<personList.length; i++) {
+            var mutter = false;
+            var vater = false;
+            var mutterId = personList[i].getAttribute("data-mutter");
+            var vaterId = personList[i].getAttribute("data-vater");
+            if (mutterId == ""){
+                mutter = true;
+            }
+            if (vaterId == "") {
+                vater = true;
+            }
+            for (var j=0; j<generationList.length-1; j++){
+                for (var k=0; k<generationList[j].length; k++){
+                    if (!mutter && generationList[j][k].getAttribute("data-id") == mutterId){
+                        mutter = true;
+                    }
+                    if (!vater && generationList[j][k].getAttribute("data-id") == vaterId) {
+                        vater = true;
+                    }
+                    if (mutter && vater){
+                        break;
+                    }
+                }
+                if (mutter && vater) {
+                    break;
+                }
+            }
+            if (mutter && vater) {
+                generationList[generationList.length-1].push(personList[i]);
+                personList.splice(i, 1);
+                i--;
+            }
+
+        }
+    }
+
+    for (var i=0; i<generationList.length; i++){
+        for (var j=0; j<generationList[i].length; j++){
+            var mutterId = generationList[i][j].getAttribute("data-mutter");
+            var vaterId = generationList[i][j].getAttribute("data-vater");
+            if (mutterId == "" && vaterId == ""){
+                continue;
+            }
+            for (var k=0; k<j; k++){
+                if (generationList[i][k].getAttribute("data-mutter") == mutterId && generationList[i][k].getAttribute("data-vater") == vaterId) {
+                    var object = generationList[i][j];
+                    generationList[i].splice(j, 1);
+                    generationList[i].splice(k+1, 0, object);
+                    break;
+                }
+            }
+        }
+    }
+
+    for (var i = 0; i < generationList.length; i++){
+        for (var j = 0; j < generationList[i].length; j++){
+            var ehepartnerId = generationList[i][j].getAttribute("data-ehepartner");
+            console.log(j);
+            if (j != 0 && j != generationList[i].length-1){
+                if (generationList[i][j - 1].getAttribute("data-id") == ehepartnerId || generationList[i][j + 1].getAttribute("data-id") == ehepartnerId){
+                    continue;
+                }
+            }
+            else if (j == 0 && generationList[i][j + 1].getAttribute("data-id") == ehepartnerId){
+                continue;
+            }
+            else if (j == generationList[i].length && generationList[i][j - 1].getAttribute("data-id") == ehepartnerId) {
+                continue;
+            }
+            for (var k = 0; k < j; k++){
+                if (generationList[i][k].getAttribute("data-id") == ehepartnerId){
+                    var object = generationList[i][k];
+                    generationList[i].splice(k, 1);
+                    generationList[i].splice(j + 1, 0, object);
+                    break;
+                }
+            }
+        }
+    }
+
+    for (var i = 0; i < generationList.length; i++) {
+        for (var j = 0; j < generationList[i].length; j++) {
+            var ehepartnerId = generationList[i][j].getAttribute("data-ehepartner");
+            if (ehepartnerId == "") {
+                continue;
+            }
+            var placed = false;
+            for (var k = i+1; k < generationList.length; k++) {
+                for (var l = 0; l < generationList[k].length; l++){
+                    if (generationList[k][l].getAttribute("data-id") == ehepartnerId){
+                        var object = generationList[i][j];
+                        generationList[i].splice(j, 1);
+                        generationList[k].splice(l + 1, 0, object);
+                        placed = true;
+                        j--;
+                        break;
+                    }
+                }
+                if (placed){
+                    break;
+                }
+            }
+        }
+    }
+
+
+    for (var i=0; i<generationList.length; i++){
+        for (var j=0; j<generationList[i].length; j++){
+            generationList[i][j].style.top = i * 100 + 50 +"px";
+            if (j==0){
+                generationList[i][j].style.left = 0 + "px";
+            }
+            else {
+                if (generationList[i][j].getAttribute("data-ehepartner") == generationList[i][j-1].getAttribute("data-id")){
+                    var elementWidth = parseInt(document.defaultView.getComputedStyle(generationList[i][j - 1]).width);
+                    var elementLeft = parseInt(document.defaultView.getComputedStyle(generationList[i][j - 1]).left);
+
+                    var newLeft = elementWidth + elementLeft;
+
+                    generationList[i][j].style.left = newLeft + "px";
+                }
+                else {
+                    var elementWidth = parseInt(document.defaultView.getComputedStyle(generationList[i][j - 1]).width);
+                    var elementLeft = parseInt(document.defaultView.getComputedStyle(generationList[i][j - 1]).left);
+
+                    var newLeft = elementWidth + elementLeft + 50;
+
+                    generationList[i][j].style.left = newLeft + "px";
+                }
+            }
+            
+        }
+    }
 }
 
 function convertAbsoluteUnitsToRelative(){
@@ -51,8 +215,6 @@ function convertAbsoluteUnitsToRelative(){
         var elementTop = parseInt(document.defaultView.getComputedStyle(element).top);
         var elementLeft = parseInt(document.defaultView.getComputedStyle(element).left);
 
-        console.log(elementWidth, elementHeight, elementTop, elementLeft);
-
         element.style.width = (elementWidth / scaleSize)*100 + '%';
         element.style.height = (elementHeight / scaleSize) * 100 + '%';
         element.style.top = (elementTop / scaleSize) * 100 + '%';
@@ -73,6 +235,41 @@ function orientateAdditionalInfo () {
         if (additionalinfoHeight >= elementTop) {
              additionalinfo.style.bottom = 0;
              additionalinfo.style.top = 100+"%";
+        }
+
+        var additionalinfoWidth = parseInt(document.defaultView.getComputedStyle(additionalinfo).width);
+
+        var elementWidth = parseInt(document.defaultView.getComputedStyle(additionalinfo.parentElement.parentElement).width);
+
+        var elementLeft = parseInt(document.defaultView.getComputedStyle(additionalinfo.parentElement.parentElement).left);
+
+        var boxsize = parseInt(document.defaultView.getComputedStyle(resize).width)
+
+        if (0.5*additionalinfoWidth > elementLeft){
+            if (elementLeft == 0){
+                additionalinfo.style.right = 100 + "%";
+                additionalinfo.style.transform = "translateX("+additionalinfoWidth+"px)";
+            }
+            else {
+                additionalinfo.style.right = 0 + "%";
+                additionalinfo.style.transform = "translateX(" + (additionalinfoWidth*0.5-elementWidth*0.5+10) +"px)";
+            }
+        }
+        else if (elementLeft + 0.5*elementWidth + 0.5*additionalinfoWidth > boxsize){
+            additionalinfo.style.right = 0 + "%";
+            additionalinfo.style.transform = "translateX("+(boxsize-(elementLeft + elementWidth)) +"px)";
+            if (elementLeft + elementWidth == boxsize) {
+                additionalinfo.style.right = 0 + "%";
+                additionalinfo.style.transform = "translateX(" + (boxsize - (elementLeft + elementWidth)) + "px)";
+            }
+            else {
+                additionalinfo.style.right = 0 + "%";
+                additionalinfo.style.transform = "translateX(" + (boxsize - (elementLeft + elementWidth)) + "px)";
+            }
+        }
+        else {
+            additionalinfo.style.right = 50 + "%";
+            additionalinfo.style.transform = "translateX(50 %)";
         }
     }    
 }
