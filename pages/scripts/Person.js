b57d05388a46cc54dd96d4bc52e0bdbe1b0191dc
Jaediknight <jaedicke.dev@gmail.com>
2021-07-13 09:38:05 +0200

    +Person.js

---
+++ pages/scripts/Person.js
@@ -0,0 +1,40 @@
+class Person {
+
+    constructor() { }
+
+    fstName(fiName) {
+        this.firstName = fiName;
+    }
+
+    famName(fName) {
+        this.familyName = fName;
+    }
+
+
+    fmother(m) {
+        this.mother = mother;
+    }
+
+    ffather(f) {
+        this.father = father;
+    }
+
+    fspouse(s) {
+        this.spouse = spouse;
+    }
+
+    fbirthPlace(bp) {
+        this.birthPlace = birthPlace;
+    }
+
+    fdeathDate(dd) {
+        this.deathDate = dd;
+    }
+
+    fbirthDate(bd) {
+        this.birhtDate = bd;
+    }
+
+
+
+}
