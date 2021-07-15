5d26a93453a7b939fbd21f649a866d7cc0a6f4c0
Eluminix <florian-kloss@hotmail.de>
2021-07-15 13:26:01 +0200

    anpassung impressum template

--- pages/scripts/templates.js
+++ pages/scripts/templates.js
@@ -1,3 +1,11 @@
+
+   const a = document.getElementsByTagName("a");
+   const contact = document.getElementsByClassName("contact");
+   const team = document.getElementsByClassName("team");
+   const stammbaum = document.getElementsByClassName("stammbaum");
+   const copyright = document.getElementsByClassName("copyright");
+
+
 function greyTheme() {
 
     var css = '.navbar a:hover, .dropdown:hover .dropbtn { background-color: #f1f1f1 }';
@@ -36,7 +44,7 @@ function greyTheme() {
     
     function lightgreenTheme() {
 
-      var ts = '.navbar a:hover, .dropdown:hover .dropbtn { background-color: #f1f1f1 }';
+      var ts = '.navbar a:hover, .dropdown:hover .dropbtn { background-color: #d0f5e5 }';
       var style = document.createElement('style');
       style.appendChild(document.createTextNode(ts));
       document.getElementsByTagName('head')[0].appendChild(style);
@@ -67,3 +75,46 @@ function greyTheme() {
       document.getElementsByClassName("footer-container")[0].style.backgroundColor = "#aaf0d1";
      
     }
+
+    function greyThemeImpressum() {
+
+      var css = '.navbar a:hover, .dropdown:hover .dropbtn { background-color: #f1f1f1 }';
+      var style = document.createElement('style');
+      style.appendChild(document.createTextNode(css));
+      document.getElementsByTagName('head')[0].appendChild(style);
+  
+      for (var i = 0; i< a.length; i++) {
+       
+        a[i].style.color = "black";
+      }
+  
+  
+      
+       
+        document.getElementById("templateNav").style.backgroundColor = "lightgrey";
+        document.getElementById("templateActive").style.backgroundColor = "grey";
+        document.getElementById("dropbtn").style.color = "black";
+      
+     
+      }
+
+      function lightgreenThemeImpressum() {
+
+        var ts = '.navbar a:hover, .dropdown:hover .dropbtn { background-color: #d0f5e5 }';
+        var style = document.createElement('style');
+        style.appendChild(document.createTextNode(ts));
+        document.getElementsByTagName('head')[0].appendChild(style);
+  
+        for (var i = 0; i< a.length; i++) {
+       
+          a[i].style.color = "black";
+        }
+      
+      
+        document.getElementById("templateNav").style.backgroundColor = "#aaf0d1";
+        document.getElementById("templateActive").style.backgroundColor = "#b3c7be";
+        document.getElementById("dropbtn").style.color = "black";
+       
+       
+      }
+
