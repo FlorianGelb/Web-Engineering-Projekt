8bf517c362de0231624479f4f23cecc3ba061076
Jaediknight <jaedicke.dev@gmail.com>
2021-07-16 14:26:57 +0200

    adjust editXml

--- pages/scripts/editXml.js
+++ pages/scripts/editXml.js
@@ -9,12 +9,31 @@ const { parseString, Builder } = require("xml2js");
 const xml = fs.readFileSync("../stammbaum.xml").toString();
 parseString(xml, function (err, data) {
 
-
-    for (let i = 0; i < Object.keys(data.stammbaum.familie).length; i++) {
-     console.log(loop(data.stammbaum.familie[i].generation, '400'));
-      
-    }
     
+
+    // console.log(data.stammbaum)
+
+    var out = {generation:[{Person:[{'$':{vorname: "Bernd", nachname:"Lee", geschlecht:"weiblich", geburtsdatum:"11-08-48", geburtsort:"Bearberry", todesdatum:"", ehepartner:"", mutter:"5", vater:"6", id:"13", familyId:""}}]}]}
+
+
+//     var out = {'$':{vorname: testPerson.vorname, nachname:"Lee", geschlecht:"weiblich", geburtsdatum:"11-08-48", geburtsort:"Bearberry", todesdatum:"", ehepartner:"", mutter:"5", vater:"6", id:"13", familyId:""}}
+//     console.log(out)
+// 
+     data.stammbaum.familie.push(out);
+
+//      console.log("------------------------------------------------------------------------------")
+    
+//      console.log(data.stammbaum.familie[0].generation[0].Person)
+
+    // console.log(data)
+
+ // Saved the XML
+ const builder = new Builder();
+ const xml = builder.buildObject(data);
+ fs.writeFileSync("../stammbaum.xml", xml, function (err, file) {
+     if (err) throw err;
+     console.log("Saved!");
+ });
 
 
 
@@ -58,6 +77,7 @@ function generatePerson(newPerson) {
 
 
         if(newPerson.mutter == "" && newPerson.vater == "" && newPerson.ehepartner == ""){
+            var newFamile = {generation:[{Person:[{'$':{vorname: newPerson.vorname, nachname: newPerson.nachname, geschlecht: newPerson.geschlecht, geburtsdatum: newPerson.geburtsdatum, geburtsort: newPerson.geburtsort, todesdatum: newPerson.todesdatum, ehepartner: newPerson.ehepartner, mutter: newPerson.mutter, vater: newPerson.vater, id: newPerson.id, familyId: newPerson.familyId}}]}]}
 
         }else if(newPerson.mutter != "" ){
             
