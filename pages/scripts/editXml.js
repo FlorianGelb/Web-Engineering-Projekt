a23339404ed2f96553e0ca0be65677e1322c9c79
Jaediknight <jaedicke.dev@gmail.com>
2021-07-17 15:50:01 +0200

    bisschen Git-Magie

--- pages/scripts/editXml.js
+++ pages/scripts/editXml.js
@@ -10,16 +10,16 @@ const xml = fs.readFileSync("../stammbaum.xml").toString();
 parseString(xml, function (err, data) {
 
     
-
+console.log(data.stammbaum)
     // console.log(data.stammbaum)
 
-    var out = {generation:[{Person:[{'$':{vorname: "Bernd", nachname:"Lee", geschlecht:"weiblich", geburtsdatum:"11-08-48", geburtsort:"Bearberry", todesdatum:"", ehepartner:"", mutter:"5", vater:"6", id:"13", familyId:""}}]}]}
+    // var out = {generation:[{Person:[{'$':{vorname: "Bernd", nachname:"Lee", geschlecht:"weiblich", geburtsdatum:"11-08-48", geburtsort:"Bearberry", todesdatum:"", ehepartner:"", mutter:"5", vater:"6", id:"13", familyId:""}}]}]}
 
 
 //     var out = {'$':{vorname: testPerson.vorname, nachname:"Lee", geschlecht:"weiblich", geburtsdatum:"11-08-48", geburtsort:"Bearberry", todesdatum:"", ehepartner:"", mutter:"5", vater:"6", id:"13", familyId:""}}
 //     console.log(out)
 // 
-     data.stammbaum.familie.push(out);
+    //  data.stammbaum.familie.push(out);
 
 //      console.log("------------------------------------------------------------------------------")
     
@@ -44,11 +44,11 @@ function compare(person, personID) {
     return person.$.id === personID;
 }
 
-function loop(generation, personID) {
-    var res = search(generation, personID);
+function loop(familie, personID) {
+    var res = search(familie, personID);
     if (res == undefined) {
-        if (generation[0].generation != undefined) {
-            return loop(generation[0].generation, personID)
+        if (familie != undefined) {
+            return loop(familie, personID)
         } else {
            
             return;
@@ -59,8 +59,8 @@ function loop(generation, personID) {
     }
 }
 
-function search(generation, personID) {
-    return generation[0].Person.find(element => compare(element, personID));
+function search(familie, personID) {
+    return familie[0].Person.find(element => compare(element, personID));
    
    
 }
@@ -77,10 +77,13 @@ function generatePerson(newPerson) {
 
 
         if(newPerson.mutter == "" && newPerson.vater == "" && newPerson.ehepartner == ""){
-            var newFamile = {generation:[{Person:[{'$':{vorname: newPerson.vorname, nachname: newPerson.nachname, geschlecht: newPerson.geschlecht, geburtsdatum: newPerson.geburtsdatum, geburtsort: newPerson.geburtsort, todesdatum: newPerson.todesdatum, ehepartner: newPerson.ehepartner, mutter: newPerson.mutter, vater: newPerson.vater, id: newPerson.id, familyId: newPerson.familyId}}]}]}
+            var newFamile = {Person:[{'$':{vorname: newPerson.vorname, nachname: newPerson.nachname, geschlecht: newPerson.geschlecht, geburtsdatum: newPerson.geburtsdatum, geburtsort: newPerson.geburtsort, todesdatum: newPerson.todesdatum, ehepartner: newPerson.ehepartner, mutter: newPerson.mutter, vater: newPerson.vater, id: newPerson.id, familyId: newPerson.familyId}}]}
 
         }else if(newPerson.mutter != "" ){
-            
+            for (let i = 0; i < data.search.length; i++) {
+                const element = array[i];
+                
+            }
         }
 
 
