2c95dabaa7ef255cace230d80ac7e711149c0926
Jaediknight <jaedicke.dev@gmail.com>
2021-07-17 15:52:44 +0200

    person hergezaubert

---
+++ pages/scripts/person.js
@@ -0,0 +1,49 @@
+class Person {
+
+    constructor() { }
+
+    vorname(firstName) {
+        this.vname = firstName;
+    }
+
+    nachname(familyName) {
+        this.nname = familyName;
+    }
+
+    geschlecht(gender){
+        this.gschlecht = gender;
+    }
+
+    geburtsdatum(birthDate) {
+        this.geburtsdat = birthDate;
+    }
+
+    geburtsort(birthPlace) {
+        this.gebort = birthPlace;
+    }
+    
+    todesdatum(deathDate) {
+        this.todesdat = deathDate;
+    }
+
+    ehepartner(spouse) {
+        this.ehepartnr = spouse;
+    }
+
+    mutter(mother) {
+        this.mttr = mother;
+    }
+
+    vater(father) {
+        this.vtr = father;
+    }
+
+    id(Id){
+        this.ID = Id;
+    }
+
+    famId(familyId){
+        this.familyId = familyId;
+    }
+
+}
