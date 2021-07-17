e7840a832ec231992812504d2cb3a23ba41f7b8e
Jaediknight <jaedicke.dev@gmail.com>
2021-07-17 22:14:06 +0200

    generatePerson() in editXml.js

--- pages/scripts/editXml.js
+++ pages/scripts/editXml.js
@@ -4,115 +4,106 @@ const fs = require("fs");
 const { parseString, Builder } = require("xml2js");
 
 
-
-// load XML and parse to JSON
-const xml = fs.readFileSync("../stammbaum.xml").toString();
-parseString(xml, function (err, data) {
-
-    
-console.log(data.stammbaum)
-    // console.log(data.stammbaum)
-
-    // var out = {generation:[{Person:[{'$':{vorname: "Bernd", nachname:"Lee", geschlecht:"weiblich", geburtsdatum:"11-08-48", geburtsort:"Bearberry", todesdatum:"", ehepartner:"", mutter:"5", vater:"6", id:"13", familyId:""}}]}]}
-
-
-//     var out = {'$':{vorname: testPerson.vorname, nachname:"Lee", geschlecht:"weiblich", geburtsdatum:"11-08-48", geburtsort:"Bearberry", todesdatum:"", ehepartner:"", mutter:"5", vater:"6", id:"13", familyId:""}}
-//     console.log(out)
-// 
-    //  data.stammbaum.familie.push(out);
-
-//      console.log("------------------------------------------------------------------------------")
-    
-//      console.log(data.stammbaum.familie[0].generation[0].Person)
-
-    // console.log(data)
-
- // Saved the XML
- const builder = new Builder();
- const xml = builder.buildObject(data);
- fs.writeFileSync("../stammbaum.xml", xml, function (err, file) {
-     if (err) throw err;
-     console.log("Saved!");
- });
-
-
-
-
-});
-
 function compare(person, personID) {
     return person.$.id === personID;
 }
 
+
 function loop(familie, personID) {
-    var res = search(familie, personID);
-    if (res == undefined) {
-        if (familie != undefined) {
-            return loop(familie, personID)
-        } else {
-           
-            return;
+    var res;
+    for (let i = 0; i < familie.length; i++) {
+        res = familie[i].Person.find(element => compare(element, personID));
+        if (res != undefined) {
+            return res;
         }
-
-    } else {
-        return res;
     }
+    return res;
 }
-
-function search(familie, personID) {
-    return familie[0].Person.find(element => compare(element, personID));
-   
-   
-}
-
-
 
 
 
 function generatePerson(newPerson) {
 
     // load XML and parse to JSON
-    const xml = fs.readFileSync("../xml-beispiel/dummydata.xml").toString();
+    const xml = fs.readFileSync("../stammbaum.xml").toString();
     parseString(xml, function (err, data) {
 
 
-        if(newPerson.mutter == "" && newPerson.vater == "" && newPerson.ehepartner == ""){
-            var newFamile = {Person:[{'$':{vorname: newPerson.vorname, nachname: newPerson.nachname, geschlecht: newPerson.geschlecht, geburtsdatum: newPerson.geburtsdatum, geburtsort: newPerson.geburtsort, todesdatum: newPerson.todesdatum, ehepartner: newPerson.ehepartner, mutter: newPerson.mutter, vater: newPerson.vater, id: newPerson.id, familyId: newPerson.familyId}}]}
-
-        }else if(newPerson.mutter != "" ){
-            for (let i = 0; i < data.search.length; i++) {
-                const element = array[i];
-                
+        if (newPerson.mutter == "" && newPerson.vater == "" && newPerson.ehepartner == "") {
+            var newFamile = { Person: [{ '$': { vorname: newPerson.vorname, nachname: newPerson.nachname, geschlecht: newPerson.geschlecht, geburtsdatum: newPerson.geburtsdatum, geburtsort: newPerson.geburtsort, todesdatum: newPerson.todesdatum, ehepartner: newPerson.ehepartner, mutter: newPerson.mutter, vater: newPerson.vater, id: newPerson.id, familyId: toString(data.stammbaum.familie.length) } }] }
+            data.stammbaum.familie.push(newFamile);
+            saveXml(data);
+            return;
+        }
+        // just mother
+        else if (newPerson.mutter != "" && newPerson.ehepartner == "") {
+            var mutter = loop(data.stammbaum.familie, newPerson.mutter);
+            if (mutter != undefined) {
+                var personToPush = { '$': { vorname: newPerson.vorname, nachname: newPerson.nachname, geschlecht: newPerson.geschlecht, geburtsdatum: newPerson.geburtsdatum, geburtsort: newPerson.geburtsort, todesdatum: newPerson.todesdatum, ehepartner: newPerson.ehepartner, mutter: newPerson.mutter, vater: newPerson.vater, id: newPerson.id, familyId: mutter.familyId } }
+                data.stammbaum.familie[parseInt(mutter.familyId)].Person.push(personToPush);
+                saveXml(data);
+                return;
+            } else {
+                alert("Die ausgewählte Mutter konnte nicht gefunden werden!");
             }
         }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        // Saved the XML
-        const builder = new Builder();
-        const xml = builder.buildObject(data);
-        fs.writeFileSync("dummydata.xml", xml, function (err, file) {
-            if (err) throw err;
-            console.log("Saved!");
-        });
+        // just father
+        else if (newPerson.vater != "" && newPerson.ehepartner == "") {
+            var vater = loop(data.stammbaum.familie, newPerson.vater);
+            if (vater != undefined) {
+                var personToPush = { '$': { vorname: newPerson.vorname, nachname: newPerson.nachname, geschlecht: newPerson.geschlecht, geburtsdatum: newPerson.geburtsdatum, geburtsort: newPerson.geburtsort, todesdatum: newPerson.todesdatum, ehepartner: newPerson.ehepartner, mutter: newPerson.mutter, vater: newPerson.vater, id: newPerson.id, familyId: vater.familyId } }
+                data.stammbaum.familie[parseInt(vater.familyId)].Person.push(personToPush);
+                saveXml(data);
+                return;
+            } else {
+                alert("Der ausgewählte Vater konnte nicht gefunden werden!");
+            }
+        }
+        else if (newPerson.ehepartner != "") {
+            var ehepartner = loop(data.stammbaum.familie, newPerson.ehepartner);
+            if (ehepartner != undefined) {
+                if (newPerson.mutter == "" && newPerson.vater == "") {
+                    var personToPush = { '$': { vorname: newPerson.vorname, nachname: newPerson.nachname, geschlecht: newPerson.geschlecht, geburtsdatum: newPerson.geburtsdatum, geburtsort: newPerson.geburtsort, todesdatum: newPerson.todesdatum, ehepartner: newPerson.ehepartner, mutter: newPerson.mutter, vater: newPerson.vater, id: newPerson.id, familyId: ehepartner.familyId } }
+                    data.stammbaum.familie[parseInt(ehepartner.familyId)].Person.push(personToPush);
+                    saveXml(data);
+                    return;
+                } else if (newPerson.mutter != "") {
+                    var mutter = loop(data.stammbaum.familie, newPerson.mutter)
+                    if (mutter != undefined) {
+                        for (let i = 0; i < data.stammbaum.familie[parseInt(mutter.familyId)].Person.length; i++) {
+                            data.stammbaum.familie[parseInt(ehepartner.familyId)].Person.push(data.stammbaum.familie[parseInt(mutter.familyId)].Person[i])
+                        }
+                        delete data.stammbaum.familie[parseInt(mutter.familyId)];
+                        saveXml(data);
+                        return;
+                    } else {
+                        alert("Fehler bei Ehepartner und Mutter")
+                    }
+                } else if (newPerson.vater != "") {
+                    var vater = loop(data.stammbaum.familie, newPerson.vater)
+                    if (vater != undefined) {
+                        for (let i = 0; i < data.stammbaum.familie[parseInt(vater.familyId)].Person.length; i++) {
+                            data.stammbaum.familie[parseInt(ehepartner.familyId)].Person.push(data.stammbaum.familie[parseInt(vater.familyId)].Person[i])
+                        }
+                        delete data.stammbaum.familie[parseInt(vater.familyId)];
+                        saveXml(data);
+                        return;
+                    } else {
+                        alert("Fehler bei Ehepartner und Vater")
+                    }
+                }
+            } else {
+                alert("Der ausgewählte Ehepartner konnte nicht gefunden werden");
+            }
+        }
     });
-
 }
 
-
-
-
-
+function saveXml(data) {
+    const builder = new Builder();
+    const xml = builder.buildObject(data);
+    fs.writeFileSync("../stammbaum.xml", xml, function (err, file) {
+        if (err) throw err;
+        console.log("Saved!");
+    });
+}
